8fcfbdb2734cab0c64914017bd4b968515f563b8
divyamaddipati <21b01a12a2@svecw.edu.in>
2023-12-12 13:30:17 +0530

    updated reactquills inport stmt, and implemented dynamic import

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -13,7 +13,6 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { app } from "@/utils/firebase";
-import ReactQuill from "react-quill";
 
 const WritePage = () => {
   const { status } = useSession();
